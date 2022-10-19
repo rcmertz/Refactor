2c3b1054eb8c40322c1839856d2e50edd34c73d6
rcmertz <rcliberalli@hotmail.com>
2022-10-19 13:03:10 -0300

    Refactor

--- refactor.js
+++ refactor.js
@@ -2,26 +2,19 @@ function returnRandomNumber() {
   return Math.random();
 }
 
-function sumToRandomNumber(num) {
-  const numberToSum = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToSum * 100) + num}!`;
+function roundNum(num){
+  return Math.round(num * 100);
 }
 
-function subtractRandomNumber(num) {
-  const numberToSubtract = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToSubtract * 100) - num}!`;
-}
-
-function multiplyToRandomNumber(num) {
-  const numberToMultiply = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToMultiply * 100) * num}!`;
-}
-
-function divideRandomNumber(num) {
-  const numberToDivide = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToDivide * 100) / num}!`;
+function randomMath(num, numCalculator, randomM){
+  switch(randomM){
+    case 1: 
+      return `Seu número é ${roundNum(numCalculator) + num}!`;
+    case 2:
+      return `Seu número é ${roundNum(numCalculator) - num}!`;
+    case 3:
+      return `Seu número é ${roundNum(numCalculator) * num}!`;
+    case 4:
+      return `Seu número é ${roundNum(numCalculator) / num}!`;
+  }
 }
